010819dd7baf8481cae43d908335f29ab5554f8b
Dhanush123 <dhanush.patel@ymail.com>
2017-01-12 16:18:20 -0800

    add more error handling
    should get accepted now

--- index.js
+++ index.js
@@ -81,6 +81,9 @@ var handlers = {
                    self.emit("Unhandled");
                   }
                 }
+                else{
+                  self.emit("Unhandled");
+                }
               });
             }
         }
